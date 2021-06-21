06d6750185ff737e7f9e845ecbc7faa9995cbe15
unknown <leonardo.costa@EXT-ENG26.exsto.local>
2021-06-21 14:44:20 -0300

    feat: create entity User

--- src/database/runMigrations.ts
+++ src/database/runMigrations.ts
@@ -5,11 +5,27 @@ import { createConnection } from './connection';
 
 (async () => {
 
-  const connection = await createConnection();
+  const { client } = await createConnection();
 
   const fileDatabaseDir = path.join(__dirname, "migrations");
 
+  console.log("Star migrations", new Date())
+
   fs.readdir(fileDatabaseDir, (err, files) => {
-    console.log(files);
+    if (err) {
+      console.error(err)
+    }
+    files.forEach(file => {
+      fs.readFile(path.join(fileDatabaseDir, file), async (err, content) => {
+        if (err) {
+          console.error(err)
+        }
+        const runMigrationQuery = content.toString();
+
+        await client.query(runMigrationQuery);
+      })
+    })
+
+    console.log("Finish migrations", new Date())
   })
 })();
